07c64222e7df74ddbb4c56d14170576feaf6efed
饶家俊 <raojiajun@bimwinner.com>
2020-03-23 10:23:22 +0800

    完成第一个路由, 四种方式(body, header, params, path)的请求参数都能正常获取

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const Koa = require('koa');
-const app = new Koa();
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
+const app = new Koa();
+const router = new Router();
+app.use(bodyParser());
+
+router.post('/:id', (ctx, next) => {
+  const path = ctx.params.id;
+  const header = ctx.req.headers;
+  const body = ctx.request.body;
+  const query = ctx.request.query;
+
+  console.log('path', JSON.stringify(path));
+  console.log('header', JSON.stringify(header));
+  console.log('body', JSON.stringify(body));
+  console.log('query', JSON.stringify(query));
+
+
+
+  ctx.body = {
+    message: 'success21'
+  };
 });
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 app.listen(3000);
