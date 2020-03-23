7ae1a8ddf472c420dfec66041b58a9fc8296cb10
饶家俊 <raojiajun@bimwinner.com>
2020-03-23 10:50:24 +0800

    使用 ts 代替 js

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-const Koa = require('koa');
-const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser');
+import * as Koa from 'koa';
+import * as Router from 'koa-router';
+import * as bodyParser from 'koa-bodyparser';
 
 const app = new Koa();
 const router = new Router();
 app.use(bodyParser());
 
-router.post('/:id', (ctx: any) => {
+router.post('/:id', (ctx: Koa.Context) => {
   const path = ctx.params.id;
   const header = ctx.req.headers;
   const body = ctx.request.body;
@@ -16,8 +16,6 @@ router.post('/:id', (ctx: any) => {
   console.log('header', JSON.stringify(header));
   console.log('body', JSON.stringify(body));
   console.log('query', JSON.stringify(query));
-
-
 
   ctx.body = {
     message: 'success21'
