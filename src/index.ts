c91d078404bf5f173c9a0d96d3b1337907aae4e3
饶家俊 <raojiajun@bimwinner.com>
2020-03-23 14:44:27 +0800

    使用工具生成 api 文档

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,40 @@ const router = new Router();
 app.use(bodyParser());
 
 // 获取所有数据
-router.get('/todos', (ctx: Koa.Context) => {
+router.get('/blogs', (ctx: Koa.Context) => {
   ctx.body = {
     message: "获取所有数据",
     data: []
   }
 });
 
-// 获取某个数据
-router.get('/todos/:id', (ctx: Koa.Context) => {
+
+/**
+ * @api {get} /blogs/:id 使用 id 获取某篇 blog
+ * @apiName getBlog
+ * @apiGroup Blog
+ *
+ * @apiParam {Number} id 想要获取 blog 的 id.
+ *
+ * @apiSuccess {String} blogTitle blog 的标题.
+ * @apiSuccess {String} blogContent blog 的内容.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "blogTitle": "blog 标题",
+ *       "blogContent": "今天天气很好"
+ *     }
+ *
+ * @apiError BlogNotFound The id of the Blog was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "BlogNotFound"
+ *     }
+ */
+router.get('/blogs/:id', (ctx: Koa.Context) => {
   ctx.body = {
     message: "获取某个数据",
     data: []
@@ -23,7 +48,7 @@ router.get('/todos/:id', (ctx: Koa.Context) => {
 });
 
 // 新增一个数据
-router.post('/todos', (ctx: Koa.Context) => {
+router.post('/blogs', (ctx: Koa.Context) => {
   ctx.body = {
     message: "新增一个数据",
     data: []
@@ -31,7 +56,7 @@ router.post('/todos', (ctx: Koa.Context) => {
 });
 
 // 修改一个数据, 用户只需要带上需要修改的字段即可
-router.patch('/todos/:id', (ctx: Koa.Context) => {
+router.patch('/blogs/:id', (ctx: Koa.Context) => {
   ctx.body = {
     message: "修改一个数据",
     data: []
@@ -39,7 +64,7 @@ router.patch('/todos/:id', (ctx: Koa.Context) => {
 });
 
 // 删除一个数据
-router.get('/todos', (ctx: Koa.Context) => {
+router.get('/blogs', (ctx: Koa.Context) => {
   ctx.body = {
     message: "删除一个数据",
     data: []
